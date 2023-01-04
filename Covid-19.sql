c3baeba552c9079cf961fdc1626e80fa2da15a80
Adewuyi kehinde <adewuyi136@gmail.com>
2023-01-04 11:17:29 -0800

    Add files via upload

--- Covid-19.sql
+++ Covid-19.sql
@@ -10,43 +10,6 @@ JOIN SYS.TYPES T
      ON C.USER_TYPE_ID=T.USER_TYPE_ID
 WHERE C.OBJECT_ID=OBJECT_ID('dbo.country_wise_latest');
 
---changing the datatypes of columns
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN Confirmed INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN Recovered INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN Active INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN Deaths INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [New cases] INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [New deaths] INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [New recovered] INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [Deaths   100 Cases] FLOAT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [Recovered   100 Cases] FLOAT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [Confirmed last week] INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [1 week change] INT;
-
-ALTER TABLE dbo.country_wise_latest
-    ALTER COLUMN [1 week % increase] FLOAT;
-
 --change the text inf in column Death-per-100-recovered to 0 in order to fix the datatype
 UPDATE dbo.country_wise_latest SET [Deaths-per-100-Recovered]= 0
 WHERE [Deaths-per-100-Recovered]= 'inf';
