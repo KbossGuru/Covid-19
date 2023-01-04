0f21e564f280ce0a72f48268a061d341cfacd7d9
Adewuyi kehinde <adewuyi136@gmail.com>
2023-01-04 11:11:50 -0800

    Add files via upload

--- Covid-19.sql
+++ Covid-19.sql
@@ -54,10 +54,42 @@ WHERE [Deaths-per-100-Recovered]= 'inf';
 ALTER TABLE dbo.country_wise_latest
     ALTER COLUMN [Deaths-per-100-Recovered] FLOAT;
 
---Countries with the highest number of cases
---countries with the highest number of deaths 
---countries with the highest recoveries
---Region with the highest number of cases
---Region with the highest number of deaths 
+SELECT * FROM dbo.country_wise_latest; 
+
+--Top 10 Countries with the highest number of cases
+SELECT [Country(Region)], Confirmed
+FROM dbo.country_wise_latest
+ORDER BY Confirmed DESC
+OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY;
+
+--Top 10 countries with the highest death rate 
+SELECT [Country(Region)], Confirmed, Deaths, ROUND(((CAST(Deaths AS FLOAT)/Confirmed)* 100),3) AS Death_rate 
+FROM dbo.country_wise_latest
+WHERE Confirmed > 1000
+ORDER BY Death_rate DESC
+OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY;
+
+--TOP 10 countries with the highest recoveries
+SELECT [Country(Region)], Confirmed, Recovered, ROUND(((CAST(Recovered AS FLOAT)/Confirmed)* 100),3) AS Recovery_rate 
+FROM dbo.country_wise_latest
+WHERE Confirmed > 1000
+ORDER BY Recovery_rate DESC
+OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY;
+
+--Regions with the highest number of cases
+SELECT [WHO-Region], SUM(Confirmed) AS total_cases
+FROM dbo.country_wise_latest
+GROUP BY [WHO-Region]
+ORDER BY total_cases DESC;
+
+--Region with the highest death rate 
+SELECT [WHO-Region], SUM(Confirmed) AS total_cases, SUM(Deaths) AS total_deaths, ROUND(((CAST(SUM(Deaths) AS FLOAT)/SUM(Confirmed))* 100),3) AS Death_rate 
+FROM dbo.country_wise_latest
+GROUP BY [WHO-Region]
+ORDER BY Death_rate DESC;
+
 --Region with the highest recoveries
---Region with the most deaths
+SELECT [WHO-Region], SUM(Confirmed) AS total_cases, SUM(Recovered) AS recovered, ROUND(((CAST(SUM(Recovered) AS FLOAT)/SUM(Confirmed))* 100),3) AS Recovery_rate
+FROM dbo.country_wise_latest
+GROUP BY [WHO-Region]
+ORDER BY Recovery_rate DESC;
